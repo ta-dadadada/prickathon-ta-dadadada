d33e2c6e83c62c935dec00c197021eaf20f1a82f
ta-dadadada <94or2.t@gmail.com>
2018-05-21 00:05:53 +0900

    refs #1 いいね機能を追加

--- src/components/CharacterDetail.vue
+++ src/components/CharacterDetail.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="character_detail">
+    {{ usersRef }}
     <img :src="characters[$route.params.id].image_url">
-    <h1>{{ characters[$route.params.id].name}}</h1>
+    <h1>{{ characters[$route.params.id].name}} ☆{{ characters[$route.params.id].favorite }}いいね☆</h1>
     <p>{{ characters[$route.params.id].msg }}</p>
+    <button v-on:click="increment($route.params.id)">いいね！</button>
   </div>
 </template>
 
@@ -16,15 +18,25 @@ export default {
           name: '桃山みらい',
           msg: 'キラッとコーデに包まれていく〜',
           image_url:
-            'http://www.tv-tokyo.co.jp/anime/prichan/images/chara/chara_mirai_illust.png'
+            'http://www.tv-tokyo.co.jp/anime/prichan/images/chara/chara_mirai_illust.png',
+          favorite: 0
         },
         emo: {
           name: '萌黄えも',
           msg: 'エモさの塊',
           image_url:
-            'http://www.tv-tokyo.co.jp/anime/prichan/images/chara/chara_emo_illust.png'
+            'http://www.tv-tokyo.co.jp/anime/prichan/images/chara/chara_emo_illust.png',
+          favorite: 0
         }
       }
+    }
+  },
+  methods: {
+    increment (id) {
+      this.characters[id].favorite++
+    },
+    reset (id) {
+      this.characters[id].favorite = 0
     }
   }
 }
